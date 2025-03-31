9ea3616468d9955b3aaca2e9ffc1415b3147008d
Hiroforstudy <hiro.forstudyingandwork@gmail.com>
2025-03-31 16:41:51 +0900

    modified url.py

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # ログアウトURL
     #path('priv', views.private_page, name='priv'),
